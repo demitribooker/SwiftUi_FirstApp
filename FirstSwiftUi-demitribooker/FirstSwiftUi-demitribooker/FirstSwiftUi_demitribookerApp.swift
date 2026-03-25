844cfe7f0db0eef9ffeb17b2c308e7b381cc2d64
demitribooker <56265623+demitribooker@users.noreply.github.com>
2026-03-25 12:55:22 -0500

    Add files via upload

---
+++ FirstSwiftUi-demitribooker/FirstSwiftUi-demitribooker/FirstSwiftUi_demitribookerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUi_demitribookerApp.swift
+//  FirstSwiftUi-demitribooker
+//
+//  Created by Booker, Demitri on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUi_demitribookerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
